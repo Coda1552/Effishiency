dba9d0f09fc3d6a2b60ad83b3e35a3a095a53006
Cody <cody.koelliker@outlook.com>
2021-12-30 04:23:02 -0700

    update to 1.18

--- src/main/java/coda/effishiency/init/FishEnchantments.java
+++ src/main/java/coda/effishiency/init/FishEnchantments.java
@@ -5,9 +5,9 @@ import coda.effishiency.enchantments.EffishiencyEnchantment;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class FishEnchantments {
     public static final DeferredRegister<Enchantment> ENCHATNMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, Effishiency.MOD_ID);
