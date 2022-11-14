99da23bf0a395f6698057c8cd69a029f55584a01
Cody <cody.koelliker@outlook.com>
2022-11-13 18:21:18 -0700

    effishiency work but it doesnt work, also i did all this on the wrong branch  :(

--- src/main/java/coda/effishiency/Effishiency.java
+++ src/main/java/coda/effishiency/Effishiency.java
@@ -2,8 +2,11 @@ package coda.effishiency;
 
 import coda.effishiency.init.FishEnchantments;
 import net.minecraft.core.NonNullList;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.common.MinecraftForge;
@@ -34,20 +37,40 @@ public class Effishiency {
 
     private void onFishCatch(ItemFishedEvent event) {
         NonNullList<ItemStack> stacks = event.getDrops();
-        InteractionHand hand = event.getPlayer().getUsedItemHand();
-        ItemStack stack = event.getPlayer().getItemInHand(hand);
-        if (EnchantmentHelper.getEnchantments(event.getPlayer().getItemInHand(hand)).containsKey(FishEnchantments.EFFISHIENCY.get())) {
-            int i = EnchantmentHelper.getItemEnchantmentLevel(FishEnchantments.EFFISHIENCY.get(), event.getPlayer().getItemInHand(hand));
-            if (stacks.iterator().next().is(ItemTags.FISHES)) {
-                int amount = event.getPlayer().getRandom().nextInt(i) + 2;
-                int damageAmount = stack.getDamageValue() + amount - 1;
-                stacks.iterator().next().setCount(amount);
-                if (!EffishiencyConfig.Common.INSTANCE.durability.get()) {
-                    stack.getItem().setDamage(stack, stack.getDamageValue());
+        Player player = event.getPlayer();
+        InteractionHand hand = player.getUsedItemHand();
+        ItemStack stack = player.getItemInHand(hand);
+        if (EnchantmentHelper.getEnchantments(player.getItemInHand(hand)).containsKey(FishEnchantments.EFFISHIENCY.get())) {
+            int i = EnchantmentHelper.getItemEnchantmentLevel(FishEnchantments.EFFISHIENCY.get(), player.getItemInHand(hand));
+            int amount = player.getRandom().nextInt(i) + 2;
+            int damageAmount = stack.getDamageValue() + amount - 1;
+
+            if (stacks.iterator().hasNext()) {
+                ItemStack loot = stacks.iterator().next();
+
+                ItemStack excess = loot.copy();
+
+                ItemEntity item = EntityType.ITEM.create(player.level);
+                item.setItem(excess);
+                item.setPos(event.getHookEntity().position());
+
+                FishingHook hook = event.getHookEntity();
+
+                double d0 = player.getX() - hook.getX();
+                double d1 = player.getY() - hook.getY();
+                double d2 = player.getZ() - hook.getZ();
+                item.setDeltaMovement(d0 * 0.1D, d1 * 0.1D + Math.sqrt(Math.sqrt(d0 * d0 + d1 * d1 + d2 * d2)) * 0.08D, d2 * 0.1D);
+
+                for (int j = 0; j < amount; j++) {
+                    player.level.addFreshEntity(item);
                 }
-                else {
-                    stack.getItem().setDamage(stack, damageAmount);
-                }
+            }
+
+            if (!EffishiencyConfig.Common.INSTANCE.durability.get()) {
+                stack.getItem().setDamage(stack, stack.getDamageValue());
+            }
+            else {
+                stack.getItem().setDamage(stack, damageAmount);
             }
         }
         else {
