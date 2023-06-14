883c67159150cdf4a95933c5e0ce690cb8d3a15f
Coda <cody.koelliker@outlook.com>
2023-06-13 23:48:31 -0600

    make effishiency incompatible with mending

--- src/main/java/coda/effishiency/enchantments/EffishiencyEnchantment.java
+++ src/main/java/coda/effishiency/enchantments/EffishiencyEnchantment.java
@@ -3,6 +3,7 @@ package coda.effishiency.enchantments;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 public class EffishiencyEnchantment extends Enchantment {
 
@@ -24,4 +25,10 @@ public class EffishiencyEnchantment extends Enchantment {
     public boolean isAllowedOnBooks() {
         return true;
     }
+
+    @Override
+    protected boolean checkCompatibility(Enchantment p_44690_) {
+        return super.checkCompatibility(p_44690_) && p_44690_ != Enchantments.MENDING;
+    }
+
 }
