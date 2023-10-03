db1a9280ddf138b60a16bc63f9a018603340e45d
Coda <cody.koelliker@outlook.com>
2023-10-02 18:15:25 -0600

    fix non-effishiency enchanted fishing rods not reeling in loot

--- src/main/java/coda/effishiency/EffishiencyLootModifier.java
+++ src/main/java/coda/effishiency/EffishiencyLootModifier.java
@@ -47,6 +47,9 @@ public class EffishiencyLootModifier extends LootModifier {
                     stack.getItem().setDamage(stack, damageAmount);
                 }
             }
+            else {
+                ret = generatedLoot;
+            }
         }
         return ret;
     }
